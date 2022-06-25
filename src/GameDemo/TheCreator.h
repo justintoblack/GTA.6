edd4b1d15352d72f953a0dd104b3ffff1b26ef6b
Fireable <1226981598@qq.com>
2022-06-25 11:22:17 +0800

    场景1.4

--- src/GameDemo/TheCreator.h
+++ src/GameDemo/TheCreator.h
@@ -44,23 +44,23 @@ public:
 	Model  _carWheelRight;
 
 	//初始化
-	void Init(PxPhysics* physics,PxScene* gScene);
+	void Init(PxPhysics* physics, PxScene* gScene);
 
 	//创建位置标志小球(static)
-	void CreateAnchorBall(PxTransform pos, PxMaterial* gMaterial,float radius);
+	void CreateAnchorBall(PxTransform pos, PxMaterial* gMaterial, float radius);
 
 	//创建位置标志立方体(static)
-	void CreateAnchorBox(PxTransform pos, PxMaterial* gMaterial, float halfX,float halfY,float halfZ);
+	void CreateAnchorBox(PxTransform pos, PxMaterial* gMaterial, float halfX, float halfY, float halfZ);
 
 	//创建动态球体(Dynamic)
 	void CreateDynamicSphere(PxTransform pos, PxMaterial* gMaterial, float radius);
 
 	//创建单个栅栏
-	void CreateBanister(PxVec3 pos,PxVec3 dir, PxMaterial* gMaterial, float halfHeight, 
-		float halfLength, float density, float force, float torque,BreakType type);
-	
+	void CreateBanister(PxVec3 pos, PxVec3 dir, PxMaterial* gMaterial, float halfHeight,
+		float halfLength, float density, float force, float torque, BreakType type);
+
 	//创建一排栅栏
-	void CreateBanisters(PxVec3 pos, PxVec3 dir, PxMaterial* gMaterial,float separate, 
+	void CreateBanisters(PxVec3 pos, PxVec3 dir, PxMaterial* gMaterial, float separate,
 		physx::PxU32 num, float halfHeight, float halfLength,
 		float density, float force, float torque);
 
@@ -69,7 +69,7 @@ public:
 		float density, float force, float torque);
 
 	//创建一排电线杆
-	void CreatePoles(PxVec3 pos, PxVec3 dir,float separate,PxU32 num, PxMaterial* gMaterial, float halfXZ, float halfHeight,
+	void CreatePoles(PxVec3 pos, PxVec3 dir, float separate, PxU32 num, PxMaterial* gMaterial, float halfXZ, float halfHeight,
 		float density, float force, float torque);
 
 	//创建减速带
